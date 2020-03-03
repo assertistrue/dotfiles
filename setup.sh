0365892c5b35ac927d90de1762c7f485df9326dd
assert.istrue <12971167+assertistrue@users.noreply.github.com>
2020-03-03 09:00:09 +0000

    rename alias file to .alias, add file exists chk

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
-source ./alias
-
+# apply global git user configuration
 git config --global user.name assert.istrue
 git config --global user.email 12971167+assertistrue@users.noreply.github.com
 git config --list
+
+
+# source .alias file to apply custom aliases
+if [ -f ~/dotfiles/.aliases ]; then
+. ~/dotfiles/.aliases
+fi
